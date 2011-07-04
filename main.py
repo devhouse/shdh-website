5e3f07e45f9ad9324dcd23f25e4cca57b0593916
Jeff Lindsay <progrium@gmail.com>
2011-07-03 20:37:50 -0700

    initial commit. lots of images!

---
+++ main.py
@@ -0,0 +1,46 @@
+from django.utils import simplejson
+from google.appengine.api import memcache
+from google.appengine.api import urlfetch
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util, template
+
+def _request(url, cache_ttl=3600, force=False):
+    request_cache_key = 'request:%s' % url
+    failure_cache_key = 'failure:%s' % url
+    resp = memcache.get(request_cache_key)
+    if force or not resp:
+        try:
+            resp = simplejson.loads(urlfetch.fetch(url).content[11:-3])
+            memcache.set(request_cache_key, resp, cache_ttl)
+            memcache.set(failure_cache_key, resp, cache_ttl*10)
+        except (ValueError, urlfetch.DownloadError), e:
+            # Not valid JSON or request timeout
+            resp = memcache.get(failure_cache_key)
+            if not resp:
+                resp = {}
+    return resp
+
+class MainHandler(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write(template.render('templates/index.html', {}))
+
+class ContentHandler(webapp.RequestHandler):
+    def get(self, page):
+        skip_cache = self.request.get('cache') == '0'
+        try:
+            page = _request('https://shdh.pbworks.com/api_v2/op/GetPage/page/%s' % page, force=skip_cache)
+            if page['folder'] != 'Website':
+                raise LookupError()
+            self.response.out.write(template.render('templates/content.html', locals()))
+        except LookupError:
+            self.error(404)
+
+def main():
+    application = webapp.WSGIApplication([
+        ('/', MainHandler),
+        ('/(.+)', ContentHandler),  ], debug=True)
+    util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
